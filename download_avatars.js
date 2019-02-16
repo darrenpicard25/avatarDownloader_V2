bb8064526f732d98f8d8f287b70dcfab3c83c48f
Darren Picard <darrenpicard25@gmail.com>
2019-02-16 20:37:51 +0000

    Request is working. Token has been added all working okay

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,21 @@
 const request = require('request');
+const GITHUB_TOKEN = require('./secrets.js');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+       // curl https://api.github.com/repos/jquery/jquery/contributors
+  let options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-agent': 'request',
+      'Authorization' : GITHUB_TOKEN.token
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-getElementsByTagName('jquery', 'jquery', (err, results) => {
+getRepoContributors ('jquery', 'jquery', (err, results) => {
   console.log('Errors: ', err);
-  console.log('Results: ', results);
+  // console.log('Results: ', results);
 });
