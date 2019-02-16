e22a2dc88e48ac33dd4a878148e986ae9fc07a40
Darren Picard <darrenpicard25@gmail.com>
2019-02-16 20:11:40 +0000

    Starting first bit of main project

---
+++ download_avatars.js
@@ -0,0 +1,10 @@
+const request = require('request');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+}
+
+getElementsByTagName('jquery', 'jquery', (err, results) => {
+  console.log('Errors: ', err);
+  console.log('Results: ', results);
+});
