ed04b4cd24efa6bba423d60c93500c214dea0555
Darren Picard <darrenpicard25@gmail.com>
2019-02-16 21:41:59 +0000

    Added dotenv functionality to project

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,19 @@
 const request = require('request');
-const GITHUB_TOKEN = require('./secrets.js');
 const fs = require('fs');
+require('dotenv').config();
 
+// This function sends the request to the specified repoOwner and name
 function getRepoContributors(repoOwner, repoName, cb) {
-       // curl https://api.github.com/repos/jquery/jquery/contributors
   let options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-agent': 'request',
-      'Authorization' : GITHUB_TOKEN.token
+      'Authorization' : process.env.token   //GitHub token added using dotenv
     }
   };
   request(options, function(err, res, body) {
     let data = JSON.parse(body);
+    //Sending data to the callback function
     cb(err, data);
   });
 }
@@ -28,8 +29,10 @@ function downloadImageByURL(url, filePath) {
        .pipe(fs.createWriteStream(filePath));
 }
 
+//Main function
 function main () {
   let [owner, repo] = process.argv.slice(2);
+  //Simply checks to ensure both values were entered. Will stop function if one or the other was not
   if (!(owner && repo)) {
     console.log('\nPlease enter the owner and repo you wish to download from in the program\n');
     return;
@@ -44,6 +47,7 @@ function main () {
   });
 }
 
+//Call of the main function
 main();
 
 
